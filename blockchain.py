c5f0cb32efdd7f6c4971f61aff95ac88d1f14326
pybot <gursur2151@gmail.com>
2018-02-07 16:36:35 +0900

    change blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,13 @@
-# coding: utf-8
+# coding:utf-8
 
 import hashlib
 import json
-import requests
+from textwrap import dedent
 from time import time
-from uuid import uuid4
 from urllib.parse import urlparse
+from uuid import uuid4
+
+import requests
 from flask import Flask, jsonify, request
 
 
@@ -16,52 +18,6 @@ class Blockchain(object):
         self.nodes = set()
 
         self.new_block(previous_hash=1, proof=100)
-
-    def new_block(self, proof, previous_hash=None):
-        block = {
-            'index': len(self.chain) + 1,
-            'timestamp': time(),
-            'transactions': self.current_transactions,
-            'proof': proof,
-            'previous_hash': previous_hash or self.hash(self.chain[-1]),
-        }
-
-        self.current_transactions = []
-
-        self.chain.append(block)
-        return block
-
-    def new_transaction(self, sender, recipient, amount):
-        self.current_transactions.append({
-            'sender': sender,
-            'recipient': recipient,
-            'amount': amount,
-        })
-
-        return self.last_block['index'] + 1
-
-    @staticmethod
-    def hash(block):
-        block_string = json.dumps(block, sort_keys=True).encode()
-        return hashlib.sha256(block_string).hexdigest()
-
-    @property
-    def last_block(self):
-        return self.chain[-1]
-
-    def proof_of_work(self, last_proof):
-        proof = 0
-        while self.valid_proof(last_proof, proof) is False:
-            proof += 1
-
-        return proof
-
-    @staticmethod
-    def valid_proof(last_proof, proof):
-        guess = f'{last_proof}{proof}'.encode()
-        guess_hash = hashlib.sha256(guess).hexdigest()
-
-        return guess_hash[:4] == "0000"
 
     def register_node(self, address):
         parsed_url = urlparse(address)
@@ -75,7 +31,7 @@ class Blockchain(object):
             block = chain[current_index]
             print(f'{last_block}')
             print(f'{block}')
-            print("\n----------\n")
+            print(f'\n----------n\')
 
             if block['previous_hash'] != self.hash(last_block):
                 return False
@@ -111,13 +67,80 @@ class Blockchain(object):
 
         return False
 
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        self.current_transactions = []
+        return block
+
+    def new_transaction(self, sender, recipient, amount):
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    @staticmethod
+    def hash(block):
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha2569block_string.hexdigest()
+
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is Flase:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+
+        return guess_hash[:4] == "0000"
+
 
 app = Flask(__name__)
-app.config['JSON_AS_ASCII'] = False
 
 node_identifire = str(uuid4()).replace('-', '')
 
 blockchain = Blockchain()
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    blockchain.new_transaction(
+        sender='0',
+        recipient=node_identifire,
+        amount=1,
+    )
+
+    block = blockchain.new_block(proof)
+
+    response = {
+        'message': '新しいブロックを採掘しました',
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 201
 
 
 @app.route('/transactions/new', methods=['POST'])
@@ -134,30 +157,6 @@ def new_transaction():
     return jsonify(response), 201
 
 
-@app.route('/mine', methods=['GET'])
-def mine():
-    last_block = blockchain.last_block
-    last_proof = last_block['proof']
-    proof = blockchain.proof_of_work(last_proof)
-
-    blockchain.new_transaction(
-        sender="0",
-        recipient=node_identifire,
-        amount=1,
-    )
-
-    block = blockchain.new_block(proof)
-
-    response = {
-        'message': '新しいブロックを採掘しました',
-        'index': block['index'],
-        'transactions': block['transactions'],
-        'proof': block['proof'],
-        'previous_hash': block['previous_hash'],
-    }
-    return jsonify(response), 200
-
-
 @app.route('/chain', methods=['GET'])
 def full_chain():
     response = {
@@ -168,19 +167,19 @@ def full_chain():
 
 
 @app.route('/nodes/register', methods=['POST'])
-def register_one():
+def register_node():
     values = request.get_json()
 
     nodes = values.get('nodes')
     if nodes is None:
-        return "Error: 有効ではないノードのリストです", 400
+        return "Eroor: 有効ではないノードのリストです", 400
 
     for node in nodes:
         blockchain.register_node(node)
 
     response = {
-        'message': '新しいノードが追加されました',
-        'total_nodes': list(blockchain.nodes),
+        'message': "新しいノードが追加されました",
+        'new_chain': list(blockchain.nodes),
     }
     return jsonify(response), 201
 
@@ -202,6 +201,5 @@ def consensus():
 
     return jsonify(response), 200
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
